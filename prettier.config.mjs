900bdda8d444dfed8be04e7cd61bfd1abe3a2dd0
tsutsu3 <tsutsu3prog@gmail.com>
2025-07-13 19:07:50 +0900

    Init project

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 120,
+  bracketSpacing: true,
+  plugins: ["prettier-plugin-organize-imports"],
+};
+
+export default config;
